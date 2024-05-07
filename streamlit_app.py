631cff787d5eff80afcac3ba8aea101503a83e57
Muhammad Haseeb <162408407+Haseeb9323@users.noreply.github.com>
2024-05-07 16:14:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.subheader('Range slider')
 
 values = st.slider(
      'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
+     0, 100, (25, 75))
 st.write('Values:', values)
 
 # Example 3
